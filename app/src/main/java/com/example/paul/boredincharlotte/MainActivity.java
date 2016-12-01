4ae7673add779449c073ffd81c34c3d9f2e0130f
Paul Janos <Paul Janos>
2016-11-30 22:01:52 -0500

    added Pluto

--- app/src/main/java/com/example/paul/boredincharlotte/MainActivity.java
+++ app/src/main/java/com/example/paul/boredincharlotte/MainActivity.java
@@ -11,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
             "Mercury",
             "Uranus",
             "Earth",
-            "Mars"
+            "Mars",
+            "Pluto"
     };
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
