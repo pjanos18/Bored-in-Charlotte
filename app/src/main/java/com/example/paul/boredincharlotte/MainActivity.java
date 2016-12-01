5d1f5af40a5d4c6989409a8501d25f244d86f4a7
Paul Janos <Paul Janos>
2016-11-30 21:55:11 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/paul/boredincharlotte/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.paul.boredincharlotte;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private String[] mPlanetTitles = {
+            "Mercury",
+            "Uranus",
+            "Earth",
+            "Mars"
+    };
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+
+    }
+
+}
