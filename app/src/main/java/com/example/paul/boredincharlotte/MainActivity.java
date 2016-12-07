7293f4ee78f6098b6e4c51a69c7476d7ed63dade
Paul Janos <Paul Janos>
2016-12-06 22:07:46 -0500

    Added categories

--- app/src/main/java/com/example/paul/boredincharlotte/MainActivity.java
+++ app/src/main/java/com/example/paul/boredincharlotte/MainActivity.java
@@ -3,20 +3,29 @@ package com.example.paul.boredincharlotte;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import static java.lang.System.out;
+
 public class MainActivity extends AppCompatActivity {
+
     private String[] mPlanetTitles = {
-            "Mercury",
-            "Uranus",
-            "Earth",
-            "Mars",
-            "Pluto",
-            "Saturn"
+            "Homepage",
+            "What to do",
+            "Food & Drinks",
+            "Charlotte Neighborhoods",
+            "History/Historical Places",
+            "Public Transportation",
+            "Surrounding Area",
+            "Sports",
+            "Other Sources"
     };
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
         mDrawerList.setAdapter(new ArrayAdapter<String>(this,
                 R.layout.drawer_list_item, mPlanetTitles));
+
+
 
     }
 
